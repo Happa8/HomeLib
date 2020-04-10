ea7901f44a6743b2d0129ea2d2a4fc472792a484
Happa8 <Happa.Eight@gmail.com>
2020-04-10 14:28:03 +0900

    [add] 各種設定ファイル等を追加

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
+const FilterWarningsPlugin = require("webpack-filter-warnings-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const FileCopy = new CopyWebpackPlugin([{
+    from: "./src/index.html",
+    to: ""
+}])
+
+var main = {
+    mode: "development",
+    target: "electron-main",
+    entry: path.join(__dirname, "src", "main.ts"),
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [{
+            test: /.ts?$/,
+            include: [path.resolve(__dirname, "src")],
+            loader: "ts-loader",
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: [".js", ".ts"]
+    },
+    node: {
+        __dirname: false
+    },
+    plugins: [
+        new FilterWarningsPlugin({
+            exclude: [
+                /mongodb/,
+                /mssql/,
+                /mysql/,
+                /mysql2/,
+                /oracledb/,
+                /pg/,
+                /pg-native/,
+                /pg-query-stream/,
+                /react-native-sqlite-storage/,
+                /redis/,
+                /sqlite3/,
+                /sql.js/,
+                /typeorm-aurora-data-api-driver/
+            ]
+        }),
+        FileCopy
+    ],
+    externals: [nodeExternals()]
+};
+
+var renderer = {
+    mode: "development",
+    target: "electron-renderer",
+    entry: path.join(__dirname, "src", "renderer", "app.tsx"),
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist", "scripts")
+    },
+    node: {
+        __dirname: false
+    },
+    module: {
+        rules: [{
+            test: /\.(tsx|ts)$/,
+            use: ["ts-loader"],
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: [".json", ".js", ".jsx", ".css", ".ts", ".tsx"]
+    },
+    externals: [nodeExternals()]
+};
+
+module.exports = [main, renderer];
