10ba1b39d08cd9f8e32cbe558fb13d79b6a22946
Happa8 <Happa.Eight@gmail.com>
2020-05-07 17:44:02 +0900

    [add] cssリセットを追加

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,10 +1,23 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import styled from "styled-components";
+
+import GlobalStyle from "./components/globalStyle";
 
 const container = document.getElementById("app");
 
+const Main = styled.main`
+  width: 100vw;
+  height: 100vh;
+`;
+
 const App: React.FC = () => {
-  return <div>Hello, world!</div>;
+  return (
+    <Main>
+      <GlobalStyle />
+      Hello, world!
+    </Main>
+  );
 };
 
 ReactDOM.render(<App />, container);
